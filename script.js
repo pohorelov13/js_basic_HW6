5413722e79742796b044afebc6c45373876c8734
Your Name <you@example.com>
2024-02-06 15:02:43 +0200

    Homework done

---
+++ script.js
@@ -0,0 +1,6 @@
+const arr = [false, 12, -5, '23424', false, 324, '', true, null, 5, -0, 0,
+    9, undefined, NaN, 6.354, undefined, 7, null];
+
+const filterArr = arr.filter(elem => elem);
+
+console.log(filterArr);
